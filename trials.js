dac61f59fe5dc8c142d0268909a41e21b351dd0d
dmcdekker <denisemdekker@gmail.com>
2018-04-26 04:05:19 +0000

    finish through showTransactions

--- trials.js
+++ trials.js
@@ -52,28 +52,83 @@ printAccountInfo(accountHolder, accountNumber, businessName)
 console.log('\n')
 
 // Add function to print all addresses, including a header
-
-
+// Print all addresses, including a header
+function showAddresses(addresses) {
+	console.log("Addresses:");
+	for (let i =0; i < addresses.length; i++) {
+	console.log(addresses[i])
+	}
+}
 
 // Add function to print phone types and numbers
-
+// Print phone types and numbers
+function showPhoneNums(phones) {
+    console.log("Phone numbers:");
+	for (let [phone, number] of phoneNumbers) {
+		console.log(`${phone}: ${number}`);
+	}
+}
 
 // ///////////////////////////////////////////////////////
 // Transactions:
 
 // Create an empty map of transactions
-
+const transactions = new Map();
 
 // Add function to add transactions
-
+function addTransactions(date, amount){
+	transactions.set(date, amount);
+}
 
 // Use the function to add transactions
+addTransactions('May-2', -500);
+addTransactions('May-13', +1200);
+addTransactions('May-15', -100);
+addTransactions('May-21', -359);
+addTransactions('May-29', +2200);
 
+console.log(transactions);
+console.log('\n')
 
 // Add function to show balance status
-
-
+function showBalanceStatus(balance){
+	console.log(`Balance: ${balance}`)
+	if (balance < 0) {
+		console.log("You are overdrawn");
+	}else if (balance > 0 && balance < 20){
+		console.log("Warning, you are close to zero balance")
+	}else{
+		console.log("Thanks for your business.")
+	}
+}
 // Add function to show transactions
+function showTransactions(map, balance){
+	console.log(`Starting balance: ${balance}`);
+	console.log("Date\t Type \t Amount \t New Balance");
+	for (let [date, change] of transactions) {
+		let type;
+		if (change < 0) {
+			type = "withdrawal";
+		} else {
+			type = "deposit";
+		}
+		balance += change;
+		balance -= getFees();
+		console.log(`${date} \t ${type} \t ${change} \t ${balance}`);
+	}
+	showBalanceStatus(balance);
+}
+
+showTransactions(transactions, 26000)
+
+// Function to get fees
+function getFees(bal) {
+	if (bal < 0) {
+		return 25;
+	} else {
+		return 0;
+	}
+}
 
 
 // ///////////////////////////////////////////////////////
